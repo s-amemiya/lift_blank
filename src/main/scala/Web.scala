ea830d28928a940d2775d25cf00f023b020c1486
Shinya Amemiya <shinya.amemiya@gmail.com>
2011-11-23 17:16:51 +0900

    update Web.scala

--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -1,29 +1,22 @@
-import org.eclipse.jetty.servlet.ServletContextHandler
-import org.eclipse.jetty.servlet.ServletHolder
-import javax.servlet.http.HttpServlet
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-import java.io.File
-import org.eclipse.jetty.server.Server
-import net.liftweb.http.LiftFilter
-import org.eclipse.jetty.servlet.FilterHolder
 import java.util.EnumSet
-import org.eclipse.jetty.servlet.DefaultServlet
+import javax.servlet.DispatcherType
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.server.nio.SelectChannelConnector
+import net.liftweb.http.LiftFilter
 
-object Web {
-  def main(args: Array[String]) {
-    val server = new Server(Integer.valueOf(System.getenv("PORT")));
-    val context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+object Main extends App {
+  val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+  val server = new Server
+  val scc = new SelectChannelConnector
+  scc.setPort(port)
+  server.setConnectors(Array(scc))
 
-    context.setContextPath("/");
-    server.setHandler(context);
+  val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+  context.addServlet(classOf[DefaultServlet], "/")
+  context.addFilter(classOf[LiftFilter], "/*", EnumSet of DispatcherType.REQUEST)
+  context.setResourceBase("src/main/webapp")
 
-    context.addServlet(new ServletHolder(new DefaultServlet), "/*");
-
-    val tFilter = new LiftFilter()
-    context.addFilter(new FilterHolder(new LiftFilter()), "/*", EnumSet.of(javax.servlet.DispatcherType.REQUEST))
-
-    server.start();
-    server.join();
-  }
+  server.start
+  server.join
 }
